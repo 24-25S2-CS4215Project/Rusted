599104c85650a1a20ffc2d358926ab7564b2c093
filbert <fillberth@gmail.com>
2025-04-10 15:01:27 +0800

    delete everything (temporarily)

--- src/__tests__/test_typechecker.ts
+++
@@ -1,8 +0,0 @@
-import { RustedTypeChecker } from "../typechecker/typechecker";
-
-
-function testTypeChecker() {
-    const typeChecker = new RustedTypeChecker();
-    const result = typeChecker.typeCheck();
-    console.log(result);
-}
