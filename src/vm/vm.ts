d46f77968dc5c20f9d3a440054c20892d8106009
filbert <fillberth@gmail.com>
2025-04-15 00:24:38 +0800

    implement simple vm instructions

--- src/vm/vm.ts
+++ src/vm/vm.ts
@@ -41,34 +41,140 @@ export class VM {
   execute_insn(insn: I.INSTR) {
     if (false) {
       // dummy branch
+    } else if (insn instanceof I.PUSH) {
+      this.execute_push_insn(insn);
+    } else if (insn instanceof I.POP) {
+      this.execute_pop_insn(insn);
+      // else if (insn instanceof I.ALLOC
+      // else if (insn instanceof I.FREE
+      // else if (insn instanceof I.STORE
+      // else if (insn instanceof I.LOAD
+    } else if (insn instanceof I.ADD) {
+      this.execute_add_insn(insn);
+    } else if (insn instanceof I.SUB) {
+      this.execute_sub_insn(insn);
+    } else if (insn instanceof I.MUL) {
+      this.execute_mul_insn(insn);
+    } else if (insn instanceof I.DIV) {
+      this.execute_div_insn(insn);
+    } else if (insn instanceof I.MOD) {
+      this.execute_mod_insn(insn);
+    } else if (insn instanceof I.LT) {
+      this.execute_lt_insn(insn);
+    } else if (insn instanceof I.GT) {
+      this.execute_gt_insn(insn);
+    } else if (insn instanceof I.EQ) {
+      this.execute_eq_insn(insn);
+    } else if (insn instanceof I.LEQ) {
+      this.execute_leq_insn(insn);
+    } else if (insn instanceof I.GEQ) {
+      this.execute_geq_insn(insn);
+    } else if (insn instanceof I.AND) {
+      this.execute_and_insn(insn);
+    } else if (insn instanceof I.OR) {
+      this.execute_or_insn(insn);
+    } else if (insn instanceof I.NOT) {
+      this.execute_not_insn(insn);
+      // else if (insn instanceof I.JMP
+      // else if (insn instanceof I.JOF
+      // else if (insn instanceof I.LABEL
+      // else if (insn instanceof I.CALL
+      // else if (insn instanceof I.RET
+    } else if (insn instanceof I.HALT) {
+      this.execute_halt_insn(insn);
     }
-    // insn instanceof I.PUSH
-    // insn instanceof I.POP
-    // insn instanceof I.ALLOC
-    // insn instanceof I.FREE
-    // insn instanceof I.STORE
-    // insn instanceof I.LOAD
-    // insn instanceof I.ADD
-    // insn instanceof I.SUB
-    // insn instanceof I.MUL
-    // insn instanceof I.DIV
-    // insn instanceof I.MOD
-    // insn instanceof I.LT
-    // insn instanceof I.GT
-    // insn instanceof I.EQ
-    // insn instanceof I.LEQ
-    // insn instanceof I.GEQ
-    // insn instanceof I.NEQ
-    // insn instanceof I.AND
-    // insn instanceof I.OR
-    // insn instanceof I.NOT
-    // insn instanceof I.JMP
-    // insn instanceof I.JOF
-    // insn instanceof I.LABEL
-    // insn instanceof I.CALL
-    // insn instanceof I.RET
-    else if (insn instanceof I.HALT) {
-      this.halted = true;
-    }
+  }
+
+  // Helper methods
+
+  execute_push_insn(insn: I.PUSH) {
+    this.memory.stack_push_i32(insn.value);
+  }
+
+  execute_pop_insn(_: I.POP) {
+    this.memory.stack_pop_i32();
+  }
+
+  execute_add_insn(_: I.ADD) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(a + b);
+  }
+
+  execute_sub_insn(_: I.SUB) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(a - b);
+  }
+
+  execute_mul_insn(_: I.MUL) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(a * b);
+  }
+
+  execute_div_insn(_: I.DIV) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    // todo: figure out DIV semantics
+    // (javascript div semantics probably outputs floats, which may not be what we actually want here)
+    this.memory.stack_push_i32(a / b);
+  }
+
+  execute_mod_insn(_: I.MOD) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(a % b);
+  }
+
+  execute_lt_insn(_: I.LT) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a < b));
+  }
+
+  execute_gt_insn(_: I.GT) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a > b));
+  }
+
+  execute_eq_insn(_: I.EQ) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a === b));
+  }
+
+  execute_leq_insn(_: I.LEQ) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a <= b));
+  }
+
+  execute_geq_insn(_: I.GEQ) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a >= b));
+  }
+
+  execute_and_insn(_: I.AND) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a && b));
+  }
+
+  execute_or_insn(_: I.OR) {
+    const b = this.memory.stack_pop_i32();
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+(a || b));
+  }
+
+  execute_not_insn(_: I.NOT) {
+    const a = this.memory.stack_pop_i32();
+    this.memory.stack_push_i32(+!a);
+  }
+
+  execute_halt_insn(_: I.HALT) {
+    this.halted = true;
   }
 }
